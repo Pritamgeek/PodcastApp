ceab98c240643d250c3669de676470fc7f2eefa8
pritamgeek <pritam@mendios.com>
2017-10-18 10:20:06 +0530

    initial commit

--- app/src/main/java/com/example/user/firstapplication/MainActivity.java
+++ app/src/main/java/com/example/user/firstapplication/MainActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.StrictMode;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,17 +18,31 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
 import com.meetme.support.fonts.FontManager;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import static com.example.user.firstapplication.R.id.email;
+import static com.example.user.firstapplication.R.id.image;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -34,16 +51,24 @@ public class MainActivity extends AppCompatActivity {
     EditText searchcontent;
     Button loginbutton;
     Button exitbutton;
+    ImageView image;
 
     private LoginButton loginButton;
     private CallbackManager callbackManager;
-
+    private ProfilePictureView profilePictureView;
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
-
+    private void setProfileToView(JSONObject jsonObject) {
+        try {
+            profilePictureView.setPresetSize(ProfilePictureView.NORMAL);
+            profilePictureView.setProfileId(jsonObject.getString("id"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
                 new AlertDialog.Builder(MainActivity.this)
                         .setIcon(android.R.drawable.ic_dialog_alert)
                         .setTitle("Closing Activity")
-                        .setMessage("Are you sure you want to close this activity?")
+                        .setMessage("Are you sure you want to close this app?")
                         .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
@@ -92,6 +117,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        searchcontent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, SearchContent.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
 
         FacebookSdk.sdkInitialize(getApplicationContext());
             callbackManager = CallbackManager.Factory.create();
@@ -100,7 +132,24 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void onSuccess(LoginResult loginResult) {
-                Intent intent = new Intent(MainActivity.this, LoggedActivity.class);
+
+                    GraphRequest request = GraphRequest.newMeRequest(
+                            loginResult.getAccessToken(),
+                            new GraphRequest.GraphJSONObjectCallback() {
+
+                                @Override
+                                public void onCompleted(JSONObject object, GraphResponse response) {
+                                    Log.v("Main", response.toString());
+                                    setProfileToView(object);
+                                }
+                            });
+                    Bundle parameters = new Bundle();
+                    parameters.putString("fields", "id,name,email,gender, birthday");
+                    request.setParameters(parameters);
+                    request.executeAsync();
+
+
+                    Intent intent = new Intent(MainActivity.this, LoggedActivity.class);
                     MainActivity.this.startActivity(intent);
 
                 }
@@ -112,7 +161,7 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void onError(FacebookException e) {
-
+                    Toast.makeText(MainActivity.this, "error to Login Facebook", Toast.LENGTH_SHORT).show();
                 }
 
 
